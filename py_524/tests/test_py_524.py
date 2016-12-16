0ad1fbdf2204aada169d9d59f658402b31a44964
David Laing <david.laing@alumni.ubc.ca>
2016-12-15 22:30:06 -0800

    added standard error tests

--- py_524/tests/test_py_524.py
+++ py_524/tests/test_py_524.py
@@ -30,3 +30,33 @@ class Test_standard_deviation:
     def test_string_convert(self):
         with pytest.raises(TypeError):
             utils.standard_deviation(["0", "1"])
+
+class Test_standard_error:
+
+    def test_math(self):
+        assert utils.standard_error([0,1]) == 0.5
+
+    def test_atleast_length_three(self):
+        assert utils.standard_error([0,1,2]) == 1
+
+    def test_neg_numbers(self):
+        assert utils.standard_error([-1, 0, 1]) == 1
+
+    def test_same_element(self):
+        assert utils.standard_error([100,100,100]) == 0
+
+    def test_too_small(self):
+        with pytest.raises(ZeroDivisionError):
+            utils.standard_error([0])
+
+    def test_is_list(self):
+        with pytest.raises(TypeError):
+            utils.standard_error(0)
+
+    def test_null(self):
+        with pytest.raises(TypeError):
+            utils.standard_error()
+
+    def test_string_convert(self):
+        with pytest.raises(TypeError):
+            utils.standard_error(["0", "1"])
