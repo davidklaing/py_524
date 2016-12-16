9db932112685b6be45d3484afccf0dd3677bfa3b
David Laing <david.laing@alumni.ubc.ca>
2016-12-15 21:34:48 -0800

    fixed test statement

--- py_524/tests/test_py_524.py
+++ py_524/tests/test_py_524.py
@@ -1,5 +1,5 @@
 import pytest
 from py_524 import utils
 
-def test_standard_deviation():
+def test_utils():
     assert standard_deviation([1,1,1]) == 0, 'standard deviation of [1,1,1] should equal 0'
